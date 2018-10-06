b11878f49e56e0003ba33592c7f7bf0445f532de
J.N. (a.k.a. Neruthes) <i@joyneop.com>
2018-10-06 07:45:36 +0000

    ignore .DS_Store

--- index.js
+++ index.js
@@ -26,6 +26,18 @@ app.cli_hide = (filename) => {
     });
 };
 
+app.cli_hide_all = (filename) => {
+    exec('ls -1"', function (err, stdout, stderr) {
+        if (stdout) {
+            console.log('Hid file%s:', stdout.trim().split('\n').length > 1 ? 's' : '');
+            console.log(stdout.trim().split('\n').map(filename => filename.replace('.0FEFC2FF211E40EA806E84205540F8B8', '')).join('\n'));
+            stdout.trim().split('\n').map(function (filename) {
+                fs.rename(filename, filename.slice(33), function () {});
+            });
+        };
+    });
+};
+
 app.cli_release = () => {
     exec('ls -1a | grep "\\.0FEFC2FF211E40EA806E84205540F8B8"', function (err, stdout, stderr) {
         if (stdout) {
